49181ec5b3a732ed80f01475542726e3396a4b68
Junfei Zhang <zhangjunfly@gmail.com>
2017-01-15 22:06:48 -0800

    first c

---
+++ app/src/main/java/edu/washington/jz39/activityspy/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.washington.jz39.activityspy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    private final static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "We're going down, Captain");
+    }
+
+//    @Override
+//    protected void onStart(){
+//        super.onStart();
+//        Log.i(TAG, "onStart event fired");
+//    }
+//
+//    @Override
+//    protected void onResume(){
+//        super.onResume();
+//        Log.i(TAG, "onResume event fired");
+//    }
+
+
+}
